1f5e3510a2abea9f12f6b42ada4b8f9d208eb0e0
fokysland <fokysland@yandex.ru>
2019-07-23 12:38:21 +0500

    linter and travis added

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
+/* eslint-disable linebreak-style */
 import readlineSync from 'readline-sync';
 
 export default () => {
-    console.log('Welcome to the Brain Games!');
-    const name = readlineSync.question('May I have your name?');
-    console.log(`Hello, ${name}!`);
-}
+  console.log('Welcome to the Brain Games!');
+  const name = readlineSync.question('May I have your name?');
+  console.log(`Hello, ${name}!`);
+};
