1b3ca2321881e4b3706d37dfbc05d0cc02621d9a
fokysland <50461190+fokysland@users.noreply.github.com>
2019-08-17 13:58:34 +0500

    Update prime.js

--- src/games/prime.js
+++ src/games/prime.js
@@ -6,18 +6,18 @@ const message = 'Answer "yes" if given number is prime. Otherwise answer "no".';
 const isPrime = (num) => {
   if (num < 2) return false;
   for (let i = 2; i <= Math.sqrt(num); i += 1) {
-    if (!(num % i)) return false;
+    if (num % i === 0) return false;
   }
   return true;
 };
 
 const getQuestionAndAnswer = () => {
-  const number = getRandom(0, 100);
-  const correctAnswer = isPrime(number) ? 'yes' : 'no';
+  const question = getRandom(0, 100);
+  const correctAnswer = isPrime(question) ? 'yes' : 'no';
 
   return {
     correctAnswer,
-    question: `${number}`,
+    question
   };
 };
 
