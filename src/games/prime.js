cfdf810f8fd8160c183eba0e7b3921cd412f08ad
fokysland <50461190+fokysland@users.noreply.github.com>
2019-08-17 13:57:00 +0500

    Update prime.js

--- src/games/prime.js
+++ src/games/prime.js
@@ -3,7 +3,6 @@ import getRandom from '../randomizer';
 
 const message = 'Answer "yes" if given number is prime. Otherwise answer "no".';
 
-
 const isPrime = (num) => {
   if (num < 2) return false;
   for (let i = 2; i <= Math.sqrt(num); i += 1) {
